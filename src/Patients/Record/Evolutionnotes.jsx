15ec021f8ac0089504c73f2243b4547865c724af
Gustavo <ggs11@hotmail.es>
2020-04-11 22:46:00 -0500

    Alta de pacientes y carga de pacientes

--- src/Patients/Record/Evolutionnotes.jsx
+++ src/Patients/Record/Evolutionnotes.jsx
@@ -9,11 +9,12 @@ import {
 import DateFnsUtils from "@date-io/date-fns";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import Fab from "@material-ui/core/Fab";
-import Evolnote from '../../Components/Evolnote';
+import Evolnote from "../../Components/Evolnote";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { useEffect } from "react";
 
+const urlBack = "http://localhost:4433/umarista-back/";
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -35,25 +36,37 @@ const useStyles = makeStyles((theme) => ({
   content: {
     width: "97%",
     display: "flex",
-    flexDirection:"column",
+    flexDirection: "column",
     justifyContent: "space-between",
     marginTop: "2%",
     marginBottom: "2%",
   },
 }));
 
-const data =[
-    {
-        fecha:"06/04/2020",
-        nota:"Hay un gran avance",
-    },
-    {
-        fecha:"07/04/2020",
-        nota:"Hay un gran avance",
-    }
-]
+const data = [
+  {
+    fecha: "06/04/2020",
+    nota: "Hay un gran avance",
+  },
+  {
+    fecha: "07/04/2020",
+    nota: "Hay un gran avance",
+  },
+];
 
-function Valnotes({ficha}) {
+function Valnotes({
+  ficha,
+  ahf,
+  anp,
+  ago,
+  pad,
+  expf,
+  post,
+  derm,
+  diag,
+  mapdol,
+  arcmov,
+}) {
   const classes = useStyles();
   const history = useHistory();
   const [values, setValues] = React.useState({
@@ -63,16 +76,46 @@ function Valnotes({ficha}) {
   const handleChange = (props) => (e) => {
     setValues({ ...values, [props]: e.target.value });
   };
-  const handleNext = () => {
-    history.push("#");
+  const handleNext = async () => {
+    //Aquí va la subida de todos los datos a la BD
+    const formData = new FormData();
+    formData.append("ficha_id", JSON.stringify(ficha));
+    formData.append("ant_her_fam", JSON.stringify(ahf));
+    formData.append("ant_no_pat", JSON.stringify(anp));
+    formData.append("ant_gin_obs", JSON.stringify(ago));
+    formData.append("padecimientos", JSON.stringify(pad));
+    formData.append("explor_fis", JSON.stringify(expf));
+    formData.append("postura", JSON.stringify(post));
+    formData.append("dermatomas", JSON.stringify(derm));
+    formData.append("diagnostico", JSON.stringify(diag));
+    formData.append("map_dol", JSON.stringify(mapdol));
+    formData.append("arc_mov", JSON.stringify(arcmov));
+    console.log(formData["ficha_id"]);
+    const response = await fetch(urlBack + "paciente_agregar.php", {
+      method: "POST",
+      body: formData,
+    });
+
+    console.log(await response.text());
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values);
   };
-  useEffect(()=>{
-    console.log({ficha});
-  })
+  useEffect(() => {
+    console.log({ ficha });
+    console.log(JSON.stringify(ficha));
+    console.log(JSON.stringify(ahf));
+    console.log(JSON.stringify(anp));
+    console.log({ ago });
+    console.log({ pad });
+    console.log({ expf });
+    console.log({ post });
+    console.log({ derm });
+    console.log({ diag });
+    console.log({ mapdol });
+    console.log({ arcmov });
+  });
   return (
     <Content nombre="Pacientes" select="nevol">
       <div
@@ -86,7 +129,12 @@ function Valnotes({ficha}) {
       >
         <Typography className={classes.title}>Notas de Evolución</Typography>
         <Card className={classes.Card}>
-          <form id="formulario" name="formulario" onSubmit={handleSubmit} className={classes.content}>
+          <form
+            id="formulario"
+            name="formulario"
+            onSubmit={handleSubmit}
+            className={classes.content}
+          >
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
               <KeyboardDatePicker
                 disableToolbar
@@ -95,7 +143,7 @@ function Valnotes({ficha}) {
                 margin="normal"
                 id="date-picker-inline"
                 label="Fecha"
-                style={{ margin: "initial",width:"150px" }}
+                style={{ margin: "initial", width: "150px" }}
                 value={values.fecha}
                 onChange={(e) => {
                   setValues({ ...values, fecha: e });
@@ -112,7 +160,7 @@ function Valnotes({ficha}) {
               multiline
               rows="12"
               onChange={handleChange("Nevolucion")}
-              style={{ width: "100%",marginTop:"2%" }}
+              style={{ width: "100%", marginTop: "2%" }}
               helperText={"Escribir detalles"}
             />
           </form>
@@ -130,9 +178,11 @@ function Valnotes({ficha}) {
             Agregar nota de evolución
           </Button>
         </Card>
-        <Typography className={classes.title}>Notas de evolución anotadas</Typography>
-        {data.map(datos=>(
-            <Evolnote data={datos}/>
+        <Typography className={classes.title}>
+          Notas de evolución anotadas
+        </Typography>
+        {data.map((datos) => (
+          <Evolnote data={datos} />
         ))}
         <Fab
           color="primary"
@@ -152,9 +202,18 @@ function Valnotes({ficha}) {
     </Content>
   );
 }
-const mapStateToProps = state => ({ ficha: state.Ficha.ficha });
-const mapDispatchToProps = state => ({});
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Valnotes);
+const mapStateToProps = (state) => ({
+  ficha: state.Ficha.ficha,
+  ahf: state.AHeredofamiliares,
+  anp: state.Cardnp,
+  ago: state.Cardgineco,
+  pad: state.Suffering,
+  expf: state.Physicalexam.Examenfisico,
+  post: state.Postura.Postura,
+  derm: state.Dermatomas,
+  diag: state.Diagnostico.Diagnostico,
+  mapdol: state.Mapadolor,
+  arcmov: state.Movearch,
+});
+const mapDispatchToProps = (state) => ({});
+export default connect(mapStateToProps, mapDispatchToProps)(Valnotes);
